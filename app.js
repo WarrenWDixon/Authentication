04752883d571d3378f737f3a2a8620a0c29df838
Warren Dixon <warrenwdixon@sbcglobal.net>
2019-05-12 16:13:36 -0500

    google broken

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose   = require('mongoose');
 const session    = require('express-session');
 const passport   = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate   = require('mongoose-findorcreate');
 
 const app = express('ejs');
 
@@ -32,16 +34,51 @@ const userSchema =  new mongoose.Schema ({
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 console.log(process.env.SECRET);
 
 const User = new mongoose.model("User", userSchema);
+
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user,done) {
+  done(null, userid);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:'https://www.googleapis.co/oauth2/v3/userinfo'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
   res.render("home");
 });
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', {failureReddirect: '/login'}),
+  function(req, res) {
+    res.redirect("/secrets");
+});
+
+app.get("/auth/google",
+  // we initiate communication with GoogleStrategy
+  passport.authenticate('google', {scope: ['profile'] })
+);
 
 app.get("/login", function(req,res) {
   res.render("login");
